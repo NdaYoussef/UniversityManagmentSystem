457688f673307fc1944ba72061471ecd5564b289
NdaYoussef <ndayou99@gmail.com>
2025-10-04 19:58:56 +0300

    Add User Account Interfaces

---
+++ UniManagementSystem.Application/Interfaces/IAccountServicecs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniManagementSystem.Application.DTOs;
+using UniManagementSystem.Application.DTOs.UserDtos;
+using UniManagementSystem.Domain.Models;
+
+namespace UniManagementSystem.Application.Interfaces
+{
+    internal interface IAccountServicecs
+    {
+        Task<ApplicationUser> GetCurrentUserAsync(string email);
+        Task<AuthDto> RegisterAsync(RegisterDto registerDto);
+        Task<AuthDto> LoginAsync(LoginDto loginDto);
+        Task<AuthDto> LogoutAsync(string email);
+    }
+}

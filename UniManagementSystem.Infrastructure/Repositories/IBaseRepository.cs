a2551bd35b2dd97fca1f9d91a8d588314cded2e5
NdaYoussef <ndayou99@gmail.com>
2025-10-04 19:43:35 +0300

    Add Generic Repository

---
+++ UniManagementSystem.Infrastructure/Repositories/IBaseRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniManagementSystem.Infrastructure.Repositories
+{
+    internal interface IBaseRepository<T> where T : class
+    {
+        Task<T> GetById(int id);
+        Task<T> GetStringId(string id);
+        Task<T> GetByName (string name);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}

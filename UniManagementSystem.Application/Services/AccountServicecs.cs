89efaa2cb6fa31e8fcb693b3fbf198de3e86776f
NdaYoussef <ndayou99@gmail.com>
2025-10-04 19:59:26 +0300

    add User Account services

---
+++ UniManagementSystem.Application/Services/AccountServicecs.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniManagementSystem.Application.DTOs;
+using UniManagementSystem.Application.DTOs.UserDtos;
+using UniManagementSystem.Application.Interfaces;
+using UniManagementSystem.Domain.Models;
+
+namespace UniManagementSystem.Application.Services
+{
+    internal class AccountServicecs : IAccountServicecs
+    {
+        public Task<ApplicationUser> GetCurrentUserAsync(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AuthDto> LoginAsync(LoginDto loginDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AuthDto> LogoutAsync(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AuthDto> RegisterAsync(RegisterDto registerDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

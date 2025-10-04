118ef0c229b0527925a7b6c99b1275286ab0c5d7
NdaYoussef <ndayou99@gmail.com>
2025-10-04 19:58:14 +0300

    Add User Dtos

---
+++ UniManagementSystem.Application/DTOs/AuthDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniManagementSystem.Application.DTOs
+{
+    internal class AuthDto
+    {
+        public string? Message { get; set; }
+        public bool IsAuthenticated { get; set; } = false;
+        public string? Token { get; set; }
+        public object? Data { get; set; }
+    }
+}
---
+++ UniManagementSystem.Application/DTOs/UserDtos/RegisterDto.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniManagementSystem.Domain.Enums;
+
+namespace UniManagementSystem.Application.DTOs.UserDtos
+{
+    internal class RegisterDto
+    {
+        [Required, MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required, MaxLength(50)]
+        public string LastName { get; set; }
+       
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Password must be between 6 and 20 characters")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Please confirm your password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmedPassword { get; set; }
+
+        public string NationalID { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public Roles Role { get; set; }
+
+    }
+}

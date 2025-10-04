6628ff0d0751906e4a76a4eaa07613affd9da8fc
NdaYoussef <ndayou99@gmail.com>
2025-10-04 18:46:45 +0300

    Seeding Roles into Database

---
+++ UniManagementSystem.Infrastructure/DBContext/SeedData.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniManagementSystem.Domain.Models;
+
+namespace UniManagementSystem.Infrastructure.DBContext
+{
+    public static class SeedData
+    {
+        public static async Task SeedRoles(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<Microsoft.AspNetCore.Identity.RoleManager<IdentityRole>>();
+            var userManager = serviceProvider.GetRequiredService<Microsoft.AspNetCore.Identity.UserManager<ApplicationUser>>();
+
+            string[] Roles = { "Admin", "Lecturer", "Student" };
+
+            foreach (var role in Roles)
+            {
+                if(! await roleManager.RoleExistsAsync(role) ) 
+                    await roleManager.CreateAsync(new IdentityRole(role));
+            }
+        }
+    }
+}

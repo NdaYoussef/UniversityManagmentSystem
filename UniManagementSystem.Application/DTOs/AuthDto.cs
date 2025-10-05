0468629319155081704adccfa1d370bab51667f6
NdaYoussef <ndayou99@gmail.com>
2025-10-05 18:48:07 +0300

    update authModel for user

--- UniManagementSystem.Application/DTOs/AuthDto.cs
+++ UniManagementSystem.Application/DTOs/AuthDto.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using UniManagementSystem.Domain.Enums;
 
 namespace UniManagementSystem.Application.DTOs
 {
@@ -12,5 +14,12 @@ namespace UniManagementSystem.Application.DTOs
         public bool IsAuthenticated { get; set; } = false;
         public string? Token { get; set; }
         public object? Data { get; set; }
+        public string? Email { get; set; }
+      //  public List <Roles>? Role { get; set; }
+        public string? UserName     { get; set; }
+        public List<string>? Roles { get;  set; }
+        [JsonIgnore]
+        public string? RefreshToken { get; set; }
+        public DateTime? RefreshTokenExpiration { get; set; }
     }
 }

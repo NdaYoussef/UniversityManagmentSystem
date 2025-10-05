5012252248c1f0690167d5b89f9eece87730d942
NdaYoussef <ndayou99@gmail.com>
2025-10-05 18:49:02 +0300

    Add login Service in Account Service

--- UniManagementSystem.Application/Services/AccountServicecs.cs
+++ UniManagementSystem.Application/Services/AccountServicecs.cs
@@ -16,19 +16,58 @@ namespace UniManagementSystem.Application.Services
     {
         private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly UserManager<ApplicationUser> _userManager;
-        public AccountServicecs(UserManager<ApplicationUser> userManager, IHttpContextAccessor httpContextAccessor)
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly ITokenService _tokenService;
+        public AccountServicecs(UserManager<ApplicationUser> userManager, IHttpContextAccessor httpContextAccessor,
+            SignInManager<ApplicationUser> signInManager,ITokenService tokenService)
         {
             _userManager = userManager;
             _httpContextAccessor = httpContextAccessor;
+            _signInManager = signInManager;
+            _tokenService = tokenService;
         }
         public Task<ApplicationUser> GetCurrentUserAsync(string email)
         {
             throw new NotImplementedException();
         }
 
-        public Task<AuthDto> LoginAsync(LoginDto loginDto)
+        public async Task<AuthDto> LoginAsync(LoginDto loginDto)
         {
-            throw new NotImplementedException();
+            var authModel = new AuthDto();
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+            if (user == null || !await _userManager.CheckPasswordAsync(user, loginDto.Password))
+            {
+                authModel.Message = "Email or Password is incorrect";
+                return authModel;
+            }
+            //2- get user roles 
+            var roles = await _userManager.GetRolesAsync(user);
+
+            //3- login success
+            var result = await _signInManager.PasswordSignInAsync(user, loginDto.Password, false, false);
+            if (result.Succeeded)
+            {
+                authModel.IsAuthenticated = true;
+                authModel.Email = user.Email;
+                authModel.UserName = user.UserName;
+                authModel.Roles =new List<string> { user.Role.ToString() };
+            }
+            //4- check and generate refreshtoken
+            if(user.RefreshTokens.Any(t=>t.IsActive))
+            {
+                var refreshToken = user.RefreshTokens.FirstOrDefault(t=>t.IsActive);
+                authModel.RefreshToken = refreshToken.Token;
+                authModel.RefreshTokenExpiration = refreshToken.ExpiresOn;
+            }
+            else
+            {
+                var newRefreshToken = _tokenService.GenerateRefreshToken();
+                authModel.RefreshToken = newRefreshToken.Token;
+                authModel.RefreshTokenExpiration = newRefreshToken.ExpiresOn;
+                user.RefreshTokens.Add(newRefreshToken);
+                await _userManager.UpdateAsync(user);
+            }
+            return authModel;
         }
 
         public Task<AuthDto> LogoutAsync(string email)

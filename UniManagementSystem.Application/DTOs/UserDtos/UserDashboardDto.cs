33efa3133a807bf51d2718e7387c0dbf4809074d
NdaYoussef <ndayou99@gmail.com>
2025-10-12 21:41:04 +0300

    Add User Dashboard Dto

---
+++ UniManagementSystem.Application/DTOs/UserDtos/UserDashboardDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniManagementSystem.Domain.Enums;
+
+namespace UniManagementSystem.Application.DTOs.UserDtos
+{
+    internal class UserDashboardDto
+    {
+        public string Id { get; set; } = default!;
+        public string UserName { get; set; } = default!;
+        public string Email { get; set; } = default!;
+        public string? ProfilePic { get; set; }
+        public string? NationalID { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int Age => (int)((DateTime.UtcNow - DateOfBirth).TotalDays / 365.25);
+        public Roles Role { get; set; } = default!;
+        public string? DepartmentName { get; set; }
+        public double? GPA { get; set; }
+        public decimal? Salary { get; set; }
+        public int CoursesCount { get; set; }
+    }
+}

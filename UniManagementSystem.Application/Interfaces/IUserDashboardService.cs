92774ca00ff4b5e1a3f0efbe34eba77baeb1fef5
NdaYoussef <ndayou99@gmail.com>
2025-10-12 21:42:14 +0300

    Add User Dashboard Interface

---
+++ UniManagementSystem.Application/Interfaces/IUserDashboardService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniManagementSystem.Application.DTOs;
+using UniManagementSystem.Application.DTOs.UserDtos;
+using UniManagementSystem.Domain.Models;
+
+namespace UniManagementSystem.Application.Interfaces
+{
+    internal interface IUserDashboardService
+    {
+        Task<AuthDto> GetUserData(string userId);
+        Task<AuthDto> UpdateUserData(ApplicationUser user);
+        Task<AuthDto> DeleteUserData(string userId);
+        Task<AuthDto> ChangeProfilePictureAsync(string userId,  IFormFile ProfileImage);
+
+        Task<(bool IsSuccess, string message)> ChangePasswordAsync(string userId, string oldPassword, string newPassword);
+    }
+}

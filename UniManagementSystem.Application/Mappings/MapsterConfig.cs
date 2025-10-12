3ac51c2251e1d043f8c9fad22dce5e6a35b91f9d
NdaYoussef <ndayou99@gmail.com>
2025-10-12 21:42:46 +0300

    Add and user mapster for mapping profiles

---
+++ UniManagementSystem.Application/Mappings/MapsterConfig.cs
@@ -0,0 +1,19 @@
+﻿using Mapster;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniManagementSystem.Application.DTOs.UserDtos;
+using UniManagementSystem.Domain.Models;
+
+namespace UniManagementSystem.Application.Mappings
+{
+    public  class MapsterConfig : IRegister
+    {
+        public  void Register(TypeAdapterConfig config) 
+        {
+            config.NewConfig<ApplicationUser, UserDashboardDto>();
+        }
+    }
+}

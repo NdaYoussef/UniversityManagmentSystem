fae3f33e5b2966ba3d193e6ff1329f7f0e987fa3
NdaYoussef <ndayou99@gmail.com>
2025-10-12 22:54:27 +0300

    Add dashboard controller

---
+++ UniManagementSystem.MVC/Controllers/DashboardController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using UniManagementSystem.Application.Interfaces;
+using UniManagementSystem.Domain.Models;
+
+namespace UniManagementSystem.MVC.Controllers
+{
+    [Authorize]
+    public class DashboardController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IUserDashboardService _userDashboard;
+
+        public DashboardController(UserManager<ApplicationUser> userManager, IUserDashboardService userDashboard)
+        {
+            _userManager = userManager;
+            _userDashboard = userDashboard;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (string.IsNullOrEmpty(userId)) return RedirectToAction("Login", "Account");
+
+            var dto = await _userDashboard.GetUserData(userId);
+            return View(dto);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ChangeProfilePicture(IFormFile profileImage)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (string.IsNullOrEmpty(userId)) return Unauthorized();
+
+            var result = await _userDashboard.ChangeProfilePictureAsync(userId, profileImage);
+            if (!result.IsAuthenticated)
+            {
+                TempData["Error"] = result.Message;
+            }
+            else
+            {
+                TempData["Success"] = result.Message;
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteAccount(string userId)
+        {
+            if (string.IsNullOrEmpty(userId)) return BadRequest();
+
+            var result = await _userDashboard.DeleteUserData(userId);
+            if (!result.IsAuthenticated)
+            {
+                TempData["Error"] = result.Message;
+                return RedirectToAction("Index");
+            }
+
+            TempData["Success"] = result.Message;
+            return RedirectToAction("Index", "Home");
+
+        }
+    }
+}

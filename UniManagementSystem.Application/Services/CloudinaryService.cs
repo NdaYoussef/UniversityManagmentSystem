e3540db18e4c92764df03750585269cad1a273f3
NdaYoussef <ndayou99@gmail.com>
2025-10-12 21:43:19 +0300

    Implement cloudinary service

---
+++ UniManagementSystem.Application/Services/CloudinaryService.cs
@@ -0,0 +1,38 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniManagementSystem.Application.Interfaces;
+
+namespace UniManagementSystem.Application.Services
+{
+    
+    internal class CloudinaryService : ICloudinaryService
+    {
+        private readonly Cloudinary _cloundinary;
+        public CloudinaryService(Cloudinary cloudinary)
+        {
+            _cloundinary = cloudinary;
+        }
+        public async Task<string> UploadImageAsync(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+                return null;
+
+            using var stream = file.OpenReadStream();
+
+            var uploadParams = new ImageUploadParams
+            {
+                File = new FileDescription(file.FileName, stream)
+                Folder = "UserProfiles"
+            };
+
+            var result = await _cloundinary.UploadAsync(uploadParams);
+            return result.SecureUrl.ToString();
+        }
+    }
+}
